6d58a32ff1380cef580eb85043b52f0ae6ac89d5
giuseppe materni <gabrieletenebra@gmail.com>
2023-05-07 13:06:05 +0200

    07-05-23 (13:06)

--- static/javascript/lang.js
+++ static/javascript/lang.js
@@ -1,11 +1,12 @@
 
 var Lang = {
     id: 'lpmx_phon_id',
-    url: "cfg/lang.csv",
+    url0: "cfg/lang.csv",
+    url1: "cfg/ldata.csv",
     left: 0,
     top: 0,
     wind: null,
-    show: function (url) {
+    show: function () {
         if (!this.wind) return;
         this.wind.show();
     },
@@ -21,35 +22,54 @@ var Lang = {
         this.setXY();
     },
     read: async function () {
-        let url = this.url;
-        const resp = await fetch(url, {
-            method: 'GET',
-            headers: { "Content-Type": "text/plain;charset=UTF-8" },
-            cache: 'default'
-        });
-        if (resp.ok) {
-            let csv_data = await resp.text();
-            let rows = csv_data.trim().split("\n");
-            return rows;
-        }
-        else {
-            alert(`${url} Not Found.`);
-            return [];
-        }
+
+        const read_csv = async (url) => {
+            const resp = await fetch(url, {
+                method: 'GET',
+                headers: { "Content-Type": "text/plain;charset=UTF-8" },
+                cache: 'default'
+            });
+            if (resp.ok) {
+                const csv_data = await resp.text();
+                const rows = csv_data.trim().split("\n");
+                return rows;
+            }
+            else {
+                alert(`${url} Not Found.`);
+                return [];
+            }
+        };
+
+        const ls = await read_csv(this.url0);
+        const ds = await read_csv(this.url1);
+        const lmax = Math.max(ls.length, ds.length);
+        const lds = new Array(lmax);
+        lds.fill("|");
+        for (let i = 0; i < ls.length; i++) {
+            const xy = lds[i].split('|');
+            lds[i] = `${ls[i]}|${xy[1]}`;
+        };
+        for (let i = 0; i < ds.length; i++) {
+            const xy = lds[i].split('|');
+            lds[i] = `${xy[0]}|${ds[i]}`;
+        };
+
+
+        return lds;
     },
     open: async function () {
         let rows = await this.read();
         let rs = [];
         for (let row of rows) {
-            let r = `<tr class="d"> <td>${row}</td> </tr> `;
+            const ld = row.split('|');
+            let r = `<tr class="d"> <td>${ld[0]}</td><td>${ld[1]}</td> </tr> `;
             rs.push(r);
         }
-        let html = `
-<table>
-<tr class='h'><td>LANG</td></tr>
-${rs.join("")}
-</table>
-     `.replace(/\s+|\[rn]/g, ' ');
+        const r=rs.join("");
+        let html = `<table><tr class='h'><td>LANG</td><td>DATA</td></tr>${r}</table>
+        `.replace(/\s+|\[rn]/g, ' ');
+
+
         if (!this.wind) {
             this.wind = UaWindowAdm.create(this.id, "lpmx_id");
             this.wind.linkToId("lpmx_pos_id", 10, 0, 0);
