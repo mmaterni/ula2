a0f72f616dd442267856b5151f28ae239240ca6f
giuseppe materni <gabrieletenebra@gmail.com>
2023-05-29 01:58:20 +0200

    29-05-23 (01:58)

--- exportdata.py
+++ exportdata.py
@@ -9,6 +9,7 @@ import os
 from ulalib.ula_setting import *
 import csv
 import pandas as pd
+from collections import OrderedDict
 
 __date__ = "27-05-2023"
 __version__ = "0.2.7"
@@ -35,34 +36,6 @@ NOUN|noun|gender|Masc,Fem,Neut
 NOUN|noun|number|Sing,Plur
 NOUN|noun|case|Nom,Acc
 
-{
-  "NOUN": {
-    "pos_name": "noun",
-    "msd_list": [
-      {
-        "msd_name": "gender",
-        "attrs": [
-          "Masc",
-          "Fem",
-          "Neut"
-        ]
-      },
-      {
-        "msd_name": "number",
-        "attrs": [
-          "Sing",
-          "Plur"
-        ]
-      },
-      {
-        "msd_name": "case",
-        "attrs": [
-          "Nom",
-          "Acc"
-        ]
-      }
-    ]
-  },
 """
 #tabella decodifica sigle => località,data
 """
@@ -70,9 +43,6 @@ g|GRENOBLE|grenoble|XII
 h|TOUR|tour|XII
 p|PARIS|paris|XIII
 v|VENEZIA|venezia|XIV
-
-val_locs=['grenoble','tour','paris','venezia']
-head_dats=['XII','XII','XIV']
 ljs={
 'g':'grenoble',
 'h':'tour',
@@ -80,70 +50,72 @@ ljs={
 'v':'venezia'
 }
 djs={
-'g':[0,'XII'],
-'h':[0,'XII'],
-'p':[1,'XIII'],
-'v':[2,'XIV']
+'g':'XII',
+'h':'XII',
+'p':'XIII',
+'v':'XIV'
 }
-
 """
 
+DATE = [
+    'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'X1', 'XII',
+    'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX'
+]
 
 class ExportData(object):
 
     def __init__(self, exp_name):
         self.exp_name = exp_name
-        self.pos_msd_json = {}
-
-        #lista di msd_name nel corpus
-        self.corpus_msd_lst = []
-        self.corpus_msd_blks = []
+        self.pma_df = None
+        self.exp_msd_columns = []
 
         # lista delle sigle nel corpus
-        self.corpus_sgs = []
-        self.text_sigla = 'x'
+        self.corpus_sigls = []
+        self.text_sigl = 'x'
 
         #sigle per esportazione
         self.head_locs = []
         self.val_locs = []
         self.head_dates = []
         self.rif_dates = []
-        self.exp_sgs = []
+        self.exp_sigls = []
         self.locjs = {}
         self.datejs = {}
+
+    #ritorna le righe con pos_name,msd_name selezionate
+    def find_msd_name_lst(self, pos, attr):
+        query = (self.pma_df['pos'] == pos) & (self.pma_df['attr'] == attr)
+        idxs = self.pma_df.loc[query].index.tolist()
+        lst = []
+        for i in idxs:
+            r = self.pma_df.loc[i].tolist()
+            self.pos_name=r[1]
+            lst.append(r[2])
+        return lst
 
     def read_pos_msd_csv(self):
         try:
             f = open(POS_MSD_CSV_PATH)
         except Exception as e:
             sys.ecit(e)
-        rows = csv.reader(f, delimiter='|')
-        self.pos_msd_json = {}
-        msd_set = set()
-        next(rows)
-        for row in rows:
+        reader = csv.reader(f, delimiter='|')
+        next(reader)
+        msd_lst = []
+        pma_rows = []
+        for row in reader:
             pos = row[0].lower()
             pos_name = row[1]
             msd_name = row[2]
-            msd_set.add(msd_name)
             attrs = row[3].split(",")
-            if pos not in self.pos_msd_json:
-                self.pos_msd_json[pos] = {"pos_name": pos_name, "msd_list": []}
-            self.pos_msd_json[pos]["msd_list"].append({
-                "msd_name": msd_name,
-                "attrs": attrs
-            })
+            msd_lst.append(msd_name)
+            for a in attrs:
+                pma_rows.append([pos, pos_name, msd_name, a])
         f.close()
-
-        # TODO check attrs
-        self.check_attrs()
-        try:
-            msd_set.remove('')
-        except KeyError:
-            pass
-        self.corpus_msd_lst = list(msd_set)
-        self.corpus_msd_lst.sort()
-        self.corpus_msd_blks = [''] * len(self.corpus_msd_lst)
+        #creadzion  DataFrame
+        columns = ["pos", "pos_name", "msd_name", "attr"]
+        self.pma_df = pd.DataFrame(pma_rows, columns=columns)
+        #lista deelle colonne  msd_name
+        self.exp_msd_columns = list(OrderedDict.fromkeys(msd_lst))
 
     def read_exp_csv(self):
         rows = []
@@ -155,10 +127,6 @@ class ExportData(object):
         except Exception as e:
             sys.exit(e)
         #set delle date
-        DATE = [
-            'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'X1', 'XII',
-            'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX'
-        ]
         ds = {r[3] for r in rows}
         # self.head_dats = [''] * len(ds)
         # label head delle date
@@ -167,7 +135,7 @@ class ExportData(object):
         self.rif_dates = [x for x in DATE if x in ds]
         for r in rows:
             sg = r[0]
-            self.exp_sgs.append(sg)
+            self.exp_sigls.append(sg)
             self.head_locs.append(r[1])
             self.val_locs.append(r[2])
             self.locjs[sg] = r[2]
@@ -186,107 +154,64 @@ class ExportData(object):
             pass
         sgs = list(st)
         sgs.sort()
-        self.corpus_sgs = sgs
+        self.corpus_sigls = sgs
 
-    #controlla attributi dulicati per pos
-    def check_attrs(self):
-        for k, v in self.pos_msd_json.items():
-            pos = k
-            pos_msd_list = v['msd_list']
-            atrr_lst = []
-            for js in pos_msd_list:
-                attrs = js['attrs']
-                atrr_lst.extend(attrs)
-            atrr_lst.sort()
-            print(atrr_lst)
-            attr_set = sorted(list(set(atrr_lst)))
-            x = False
-            for a in attr_set:
-                n = atrr_lst.count(a)
-                if n > 1:
-                    x = True
-                    print(pos, n, a)
-            if x:
-                pass
-                # print("|".join(atrr_lst))
-                # input('')
-
-    #nella riga località e date derivate dalla sigla
+    # località e date derivate dalla sigla 
     # venezia, ,paris, , XI,..XIV
-    def build_row_loc_dat(self, r_sgs):
+    def set_sigl_to_loc_dat(self, sigls):
         #valopri loc di riga
-        r_locs = [self.locjs[x] if x in r_sgs else '' for x in self.exp_sgs]
+        locs = [self.locjs[x] if x in sigls else '' for x in self.exp_sigls]
         #valori data di riga
-        ds = {self.datejs[x] for x in r_sgs}
-        r_dates = [x if x in ds else '' for x in self.rif_dates]
-        row = r_locs + r_dates
+        ds = {self.datejs[x] for x in sigls}
+        dates = [x if x in ds else '' for x in self.rif_dates]
+        row = locs + dates
         return row
 
-    def build_row_msd(self, pos, row_attrs):
-        pos_js = self.pos_msd_json[pos]
-        pos_msd_list = pos_js['msd_list']
-        row_msds = self.corpus_msd_blks.copy()
+    #distribuisce i valori di msd(li attrs)nelle colonne msd dell'esportazione
+    def set_attr_to_msd_columns(self, pos, row_attrs):
+        columns_msd_vals = [''] * len(self.exp_msd_columns)
         for i, attr in enumerate(row_attrs):
-            for js in pos_msd_list:
-                msd_name = js['msd_name']
-                msd_attrs = js['attrs']
-                msd_attrs_lower = [x.lower() for x in msd_attrs]
-                #atttributo di riga appartien agli atattrs  del msd corrente
-                attr_lower = attr.lower()
-                if attr_lower in msd_attrs_lower:
+            #controllare le righe trovate
+            rs = self.find_msd_name_lst(pos, attr)
+            msd_name = rs[0]
+            idx = self.exp_msd_columns.index(msd_name)
+            columns_msd_vals[idx] = attr
+        # print(self.exp_msd_columns)
+        print(pos, row_attrs)
+        print(columns_msd_vals, '\n')
+        return columns_msd_vals
 
-                    #TODO controllo attr duplicati
-                    #gestione attr duplicati in lista per pos
-                    if attr_lower == 'ind' and i == 1:
-                        print(attr)
-                        continue
-                    if attr_lower == 'imp' and i == 2:
-                        print(attr)
-                        continue
-                    #setta nella lista attrs da esportare l'attr di riga
-                    #alla posizione del nome msd corrispondente
-                    idx = self.corpus_msd_lst.index(msd_name)
-                    row_msds[idx] = attr
-                    break
-        return row_msds
-
-    # aggiunge le sigle ordinate alla row e inserisce attrs
-    # aggiunge località e data deelle sigle dei testimoni
-    def build_row(self, r):
-
-        #sigle della riga
-        r_sgs = r[SIGLA].split(',')
-        r_sgs = [x for x in r_sgs if x != '']
-
-        #distribuisce le sigle di riga nella lista delle sigle del corpus
-        row_sgs = [x if x in r_sgs else '' for x in self.corpus_sgs]
-
-        #attributi di riga escluso '' e resi minuscoli
-        row_attrs = r[MSD].split(',')
-        row_attrs = [x for x in row_attrs if x != '']
-
-        pos = r[POS].lower()
+    # setta la riga per l'esportazione
+    def build_row(self, row):
+        pos = row[POS].lower()
         if pos == '':
             return None
+        #sigle della riga
+        sigls = row[SIGLA].split(',')
+        sigls = [x for x in sigls if x != '']
+        #distribuisce le sigle di riga nella lista delle sigle del corpus
+        row_sigls = [x if x in sigls else '' for x in self.corpus_sigls]
+
+        #attributi di riga escluso '
+        row_attrs = row[MSD].split(',')
+        row_attrs = [x for x in row_attrs if x != '']
 
         #assenazione valori attributi alle colonne msd
-        row_msds = self.build_row_msd(pos, row_attrs)
+        row_msd_vals = self.set_attr_to_msd_columns(pos, row_attrs)
 
         # separazione loc, data in  LANG
-        l_d = f"{r[LANG]},,".split(',')
+        l_d = f"{row[LANG]},,".split(',')
         lang = l_d[0]
         data = l_d[1]
 
-        #assegnazione pos_name
-        pos_name = self.pos_msd_json[pos]['pos_name']
-
         #aggiunta località e date testimone
-        row_loc_dat = self.build_row_loc_dat(r_sgs)
+        row_loc_dat = self.set_sigl_to_loc_dat(sigls)
 
         #["FORMA", "LEMMA", "ETIMO", "LANG", "POS", "FUNCT"],MSDS,SIGLE ..,LOC,DATE..
         row_exp = [
-            r[FORMA], r[LEMMA], r[ETIMO], lang, data, pos_name, r[FUNCT]
-        ] + row_msds + row_sgs + row_loc_dat
+            row[FORMA], row[LEMMA], row[ETIMO], lang, data, self.pos_name,
+            row[FUNCT]
+        ] + row_msd_vals + row_sigls + row_loc_dat
         return row_exp
 
     def export_corpus(self):
@@ -319,11 +244,12 @@ class ExportData(object):
             #intestazione comprensiva delle sigle e msd
             # TODO msd maiuscole
             # attrs_head = [x.upper() for x in self.corpus_msd_lst]
-            attrs_head = self.corpus_msd_lst
+            # attrs_head = self.corpus_msd_lst
+            attrs_head = self.exp_msd_columns
             # FORMA,LEMMA,ETIMO,LANG,DATTE,POS,FUNCT,msda,...,loc,...,date,...
             head = [
                 "FORMA", "LEMMA", "ETIMO", "LANG", "DATE", "POS", "FUNCT"
-            ] + attrs_head + self.corpus_sgs + self.head_locs + self.head_dates
+            ] + attrs_head + self.corpus_sigls + self.head_locs + self.head_dates
             writer.writerow(head)
 
             #scrittura rows
@@ -362,7 +288,7 @@ class ExportData(object):
         tab2 = tab2.rename(columns={1: 'col2'})
         tab12 = pd.merge(tab1, tab2, on='col2', how='left')
         tab12 = tab12.drop(tab12.columns[[1, 2]], axis=1)
-        tab12[''] = self.text_sigla
+        tab12[''] = self.text_sigl
         tab12 = tab12.fillna('')
         #attrs in minuscolo
         tab12.iloc[:, 6] = tab12.iloc[:, 6].str.lower()
--- xx.py
+++ xx.py
@@ -1,12 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-def l1l2(lista_1, lista_2):
-    # risultato = [x if x in lista_2 else '' for x in lista_1]
-    rs = [() for x in lista_1]
-    return rs
+# def handle_duplicates(lst):
+#     n = {x: 0 for x in set(lst)}
+#     row = []
+#     for x in lst:
+#         n[x] += 1
+#         row.append(f"{x}.{n[x]}")
+#     return row
 
+# my_list = ['a', 'b', 'c', 'a', 'b', 'a']
+# result_list = handle_duplicates(my_list)
+# print(result_list)
 
-l1 = ['pippo', 'lillo', 'pluto', 'fido', 'argo', 'cesare']
-l2 = ['cesare', 'fido', 'lillo']
+# def handle_duplicates(lst):
+#     counts = {}
+#     return list(
+#         map(
+#             lambda x: f"{x}.{counts.setdefault(x, 0) + 1}"
+#             if x in counts else x, lst))
 
-l=l1l2(l1,l2)
-print(l)
+# my_list = ['a', 'b', 'c', 'a', 'b', 'a']
+# result_list = handle_duplicates(my_list)
+# print(result_list)
+
+# def handle_duplicates(lst):
+#     n = {}
+#     return [
+#         f"{x}.{n.setdefault(x, 0) + 1}" if (n[x] := n.get(x)) else
+#         (n.update({x: 1}), x) for x in lst
+#     ]
+
+# # Esempio di utilizzo
+# my_list = ['a', 'b', 'c', 'a', 'b', 'a']
+# result_list = handle_duplicates(my_list)
+# print(result_list)
+# def rimuovi_duplicati(lista):
+#     lista_senza_duplicati = []
+#     [lista_senza_duplicati.append(elemento) for elemento in lista if elemento not in lista_senza_duplicati]
+#     return lista_senza_duplicati
+
+# lista_input = ['a', 'a', 'a', 'b', 'c', 'c', 'c']
+# lista_output = rimuovi_duplicati(lista_input)
+# print(lista_output)
+
+# def rimuovi_duplicati(lista):
+#     return [elemento for elemento in lista if lista.count(elemento) == 1]
+
+# lista_input = ['a', 'a', 'a', 'b', 'c', 'c', 'c']
+# lista_output = rimuovi_duplicati(lista_input)
+# print(lista_output)
+
+from collections import OrderedDict
+
+def rimuovi_duplicati(lista):
+    # print(OrderedDict.fromkeys(lista))
+    # lst=[x for x in OrderedDict.fromkeys(lista,'')]
+    # return lst
+    return list(OrderedDict.fromkeys(lista))
+
+lista_input = ['x','b', 'b', 'b', 'a', 'c', 'c', 'c']
+lista_output = rimuovi_duplicati(lista_input)
+print(lista_output)
+
